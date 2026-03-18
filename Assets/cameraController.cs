95ab968d8e43e3bf383116c45ff4fff60b596733
SHERWIN JOHN V. TARQUINI <tarquinisherwin4@gmail.com>
2026-03-18 11:29:33 +0800

    complete rts camera movment

--- Assets/cameraController.cs
+++ Assets/cameraController.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float panSpeed = 140f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+
+    public   float scrollSpeed = 80f;
+    public float minY = 20f;
+    public float maxY = 120f;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
     }
 }
