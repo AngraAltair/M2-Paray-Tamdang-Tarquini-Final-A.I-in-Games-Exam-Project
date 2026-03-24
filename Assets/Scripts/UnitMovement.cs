c40a5f5de446d690ba4fb6afdf7f3e9a71588144
Ren Tamdang <tamdangren@gmail.com>
2026-03-25 02:57:14 +0800

    Unit selection and movement
    Tutorial vids 1-3

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour
+{
+    Camera cam;
+    NavMeshAgent agent;
+    public LayerMask ground;
+
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
