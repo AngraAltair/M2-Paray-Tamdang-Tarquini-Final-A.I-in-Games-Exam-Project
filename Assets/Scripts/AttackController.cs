34bd206101c8d4abd151d029e9e75053458dd631
Ren Tamdang <tamdangren@gmail.com>
2026-03-25 14:52:31 +0800

    Animator controller and submachine state (Ep 5 done)

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    public Transform targetToAttack;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && targetToAttack == null)
+        {
+            targetToAttack = other.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy") && targetToAttack != null)
+        {
+            targetToAttack = null;
+        }
+    }
+}
---
+++ Assets/UnitFollowState.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitFollowState : StateMachineBehaviour
+{
+    AttackController attackController;
+    NavMeshAgent agent;
+    public float attackingDistance = 1f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        attackController = animator.transform.GetComponent<AttackController>();
+        agent = animator.transform.GetComponent<NavMeshAgent>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ///Should unit transition to Idle state?
+        if (attackController.targetToAttack == null)
+        {
+            animator.SetBool("isFollowing", false);
+        }
+
+        ///Moving Unit towards enemy
+        agent.SetDestination(attackController.targetToAttack.position);
+        animator.transform.LookAt(attackController.targetToAttack);
+
+        ///Should unit transition to Attack state?
+        // float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
+        // if (distanceFromTarget < attackingDistance)
+        // {
+        //     animator.SetBool("isAttacking", true);
+        // }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(animator.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
